9df5a566bba78e99a57e058096d6e1f0ffcc0900
stefdelec <stephane@arianee.org>
2020-11-03 13:46:38 +0100

    feat(api): send body

--- features/dev-api.feature
+++ features/dev-api.feature
@@ -1,26 +1,44 @@
 Feature: Dev can test a lot of thing
 
   Scenario: Dev can call GET api
-    Then _api user can 'http 200' with api call:
+    Given _api user can 'http 200' with api call:
       | url              | https://httpbin.org/get |
       | x-api-key        | 123456                  |
       | method           | GET                     |
+
+    When _api user make call 'http 200'
+
     Then _api result status of 'http 200' should be:
       | status           | 200                     |
 
   Scenario: Dev can call GET api
-    Then _api user can 'http 404' with api call:
+    Given _api user can 'http 404' with api call:
       | url              | https://httpbin.org/gt |
       | x-api-key        | 123456                  |
       | method           | GET                     |
+
+    When _api user make call 'http 404'
+
     Then _api result status of 'http 404' should be:
       | status           | 404                     |
 
+    @dev
   Scenario: Dev can call POST api
-    Then _api user can 'http 200' with api call:
+    Given _api user can 'http 200' with api call:
       | url              | https://httpbin.org/post |
       | x-api-key        | 123456                   |
       | method           | POST                     |
+
+    Given _api body of 'http 200' is:
+      """
+     {
+     "Breaking-Bad":"<3",
+     "X-API-Key":"abcdef12345"
+     }
+    """
+
+    When _api user make call 'http 200'
+
     Then _api result status of 'http 200' should be:
       | status           | 200                     |
     Then _api result body of 'http 200' should be:
--- src/steps/api.step.ts
+++ src/steps/api.step.ts
@@ -3,6 +3,13 @@ import fetch, {Headers} from 'node-fetch';
 import assert = require("assert");
 
 
+Then('_api body of {string} is:', async function (title, body) {
+    this.apiCall[title] = {
+        ...this.apiCall[title],
+        body: JSON.stringify(JSON.parse(body))
+    };
+});
+
 Then('_api user can {string} with api call:', async function (title, tableForm) {
     const properties = tableForm.raw();
 
@@ -12,14 +19,27 @@ Then('_api user can {string} with api call:', async function (title, tableForm) 
         return acc;
     }, {});
 
-    const {url, method, body, ...headers} = prepareProperties;
+    const {url, method, ...headers} = prepareProperties;
 
     if (!url || !method) {
         console.error(prepareProperties);
         throw new Error('method and url should be defined');
     }
 
-    this.apiResult[title] = await fetch(url, {method, body, headers: new Headers(headers)});
+    this.apiCall[title] = {
+        headers: new Headers({
+            'Content-Type': 'application/json',
+            ...headers
+        }),
+        url,
+        method
+    };
+});
+
+Then('_api user make call {string}', async function (title) {
+    const {url, method, body, headers} = this.apiCall[title];
+
+    this.apiResult[title] = await fetch(url, {method, body, headers});
 });
 
 Then('_api result status of {string} should be:', async function (title, tableForm) {
@@ -33,7 +53,7 @@ Then('_api result status of {string} should be:', async function (title, tableFo
     const apiResult = this.apiResult[title];
 
     prepareProperties.forEach(([name, value]) => {
-        assert(apiResult[name].toString() === value.toString());
+        assert(apiResult[name].toString() === value.toString(), `${name}: ${apiResult[name].toString()} is not ${value.toString()}`);
     })
 });
 
@@ -48,7 +68,7 @@ Then('_api result body of {string} should be:', async function (title, tableForm
     const apiResult = await this.apiResult[title].json();
 
     prepareProperties.forEach(([name, value]) => {
-        assert(apiResult[name].toString() === value.toString());
+        assert(apiResult[name].toString() === value.toString(), `${name}: ${apiResult[name].toString()} is not ${value.toString()}`);
     })
 });
 
