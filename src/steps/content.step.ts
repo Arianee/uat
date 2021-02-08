4177790ff78d990c157dc2bd880a8eaaa96d5c0d
stefdelec <stephane@arianee.org>
2021-02-08 16:59:40 +0100

    feat(content): wait for selector to be visible before checking

--- src/steps/content.step.ts
+++ src/steps/content.step.ts
@@ -2,6 +2,11 @@ import {Then} from "cucumber";
 import assert = require("assert");
 
 Then('_{selector} content should contain {string}', async function (selectorName, expectedValue) {
+    await this.page.waitForSelector(selectorName, {
+        visible: true,
+        timeout: 5000
+    });
+
     const content = await this.page.evaluate((aselector) => {
         const element = document.querySelector(aselector);
         return element.innerHTML;
@@ -11,12 +16,17 @@ Then('_{selector} content should contain {string}', async function (selectorName
 });
 
 Then('_{selector} content should not contain {string}', async function (selectorName, expectedValue) {
-    const concatenatedSelector = this.utils.selectorFactory(selectorName);
+
+
+    await this.page.waitForSelector(selectorName, {
+        visible: true,
+        timeout: 5000
+    });
+
     const content = await this.page.evaluate((aselector) => {
         const element = document.querySelector(aselector);
         return element.innerHTML;
-    }, concatenatedSelector);
-
+    }, selectorName);
     assert(!content.trim().toLowerCase().includes(expectedValue.trim().toLowerCase()));
 });
 
