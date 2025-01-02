0f8442735bb0aed3a615e488bde0e5ca15a8803d
alexishecf <90714480+alexishecf@users.noreply.github.com>
2025-01-02 16:57:27 +0100

    fix: escape regex chars to suppot complexe urls (with query params etc

---
+++ src/steps/helpers/genericRouteToRegExp/genericRouteToRegExp.ts
@@ -0,0 +1,16 @@
+function escapeRegExp(str) {
+    return str.replace(/[?.]/g, '\\$&');
+  }
+  
+export function genericRouteToRegExp(route):RegExp {
+    let escapedRoute = escapeRegExp(route);
+    let regExpStr = escapedRoute
+      .replace('**', '{DOUBLE_WILDCARD}')
+      .replace(/\*/g, '[a-zA-Z0-9@:%._\\-\\+~#=?&]*')
+      .replace(
+        '{DOUBLE_WILDCARD}',
+        '[(http(s)?):\\/\\/(www.)?a-zA-Z0-9@:%._\\-\\+~#=]{2,256}\\.{0,1}[a-z]{0,6}\\/*'
+      );
+  
+    return new RegExp('^' + regExpStr + '/*$');
+  }
