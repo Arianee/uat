f054311950c82796218ef8cc13f0794c53300997
stefdelec <s.delecroix@gmail.com>
2025-01-03 17:11:57 +0100

    feat(httpMock): http mock works with query params

--- src/steps/helpers/genericRouteToRegExp/genericRouteToRegExp.test.ts
+++ src/steps/helpers/genericRouteToRegExp/genericRouteToRegExp.test.ts
@@ -1,4 +1,4 @@
-import {genericRouteToRegExp} from './genericRouteToRegExp';
+import { genericRouteToRegExp } from './genericRouteToRegExp';
 
 describe('genericRouteToRegExp', () => {
   test('should convert a route with single wildcard (*) to a valid RegExp', () => {
@@ -52,4 +52,34 @@ describe('genericRouteToRegExp', () => {
     expect('/api/v1/resource?query=val&another=val').toMatch(regExp);
     expect('/api/v1/resource?query=val').not.toMatch(regExp);
   });
+
+  describe('query params', () => {
+    test('with **', () => {
+      const route = "https://api.bdh-stef.arianee.com/certificate/v2/**"
+
+      const regExp = genericRouteToRegExp(route);
+      const route2 = "https://api.bdh-stef.arianee.com/certificate/v2/search?limit=1&q=%7B%22must%22%3A%5B%7B%22queryString%22%3A%7B%22defaultPath%22%3A%22tags%22%2C%22query%22%3A%22123456%22%7D%7D%5D%7D"
+      expect(route2).toMatch(regExp);
+    })
+
+    test('exact matching', () => {
+      const route = "https://api.bdh-stef.arianee.com/certificate/v2/search?limit=1&q=%7B%22must%22%3A%5B%7B%22queryString%22%3A%7B%22defaultPath%22%3A%22tags%22%2C%22query%22%3A%22123456%22%7D%7D%5D%7D"
+      const regExp = genericRouteToRegExp(route);
+      expect(route).toMatch(regExp);
+    })
+      test('exact matching', () => {
+      const route = "https://api.bdh-stef.arianee.com/certificate/v2/search?limit=1&q=%7B%22must%22%3A%5B%7B%22queryString%22%3A%7B%22defaultPath%22%3A%22tags%22%2C%22query%22%3A%22123456%22%7D%7D%5D%7D"
+      const regExp = genericRouteToRegExp(route);
+      expect(route).toMatch(regExp);
+    })
+    test('not matching', () => {
+      const route = "https://api.bdh-stef.arianee.com/certificate/v2/**"
+
+      const regExp = genericRouteToRegExp(route);
+      const route2 = "https://api.bdh-stef.arianee.com/certificate/v3/search?limit=1&q=%7B%22must%22%3A%5B%7B%22queryString%22%3A%7B%22defaultPath%22%3A%22tags%22%2C%22query%22%3A%22123456%22%7D%7D%5D%7D"
+      expect(route2).not.toMatch(regExp);
+    })
+
+  })
+
 });
--- src/steps/helpers/genericRouteToRegExp/genericRouteToRegExp.ts
+++ src/steps/helpers/genericRouteToRegExp/genericRouteToRegExp.ts
@@ -1,16 +1,18 @@
 function escapeRegExp(str) {
-    return str.replace(/[?.]/g, '\\$&');
-  }
-  
-export function genericRouteToRegExp(route):RegExp {
-    let escapedRoute = escapeRegExp(route);
-    let regExpStr = escapedRoute
-      .replace('**', '{DOUBLE_WILDCARD}')
-      .replace(/\*/g, '[a-zA-Z0-9@:%._\\-\\+~#=?&]*')
-      .replace(
-        '{DOUBLE_WILDCARD}',
-        '[(http(s)?):\\/\\/(www.)?a-zA-Z0-9@:%._\\-\\+~#=]{2,256}\\.{0,1}[a-z]{0,6}\\/*'
-      );
-  
-    return new RegExp('^' + regExpStr + '/*$');
-  }
+  return str.replace(/[?.]/g, '\\$&');
+}
+
+export function genericRouteToRegExp(route): RegExp {
+  let escapedRoute = escapeRegExp(route);
+  let regExpStr = escapedRoute
+    .replace('**', '{DOUBLE_WILDCARD}')
+    .replace(/\*/g, '[a-zA-Z0-9@:%._\\-\\+~#=?&]*')
+    .replace(
+      '{DOUBLE_WILDCARD}',
+      '[(http(s)?):\\/\\/(www.)?a-zA-Z0-9@:%._\\-\\+~#=]{2,256}\\.{0,1}[a-z]{0,6}\\/*'
+    );
+
+  regExpStr += '(\\?.*)?(#.*)?';
+
+  return new RegExp('^' + regExpStr + '/*$');
+}
